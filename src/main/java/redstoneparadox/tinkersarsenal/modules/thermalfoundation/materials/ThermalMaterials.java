e34c510d7104f6bf540541f90da98a6ff803e03b
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-22 09:46:34 -0800

    Removed Thermal Expansion materials that were not specific to TE

--- src/main/java/redstoneparadox/tinkersarsenal/modules/thermalfoundation/materials/ThermalMaterials.java
+++ src/main/java/redstoneparadox/tinkersarsenal/modules/thermalfoundation/materials/ThermalMaterials.java
@@ -11,53 +11,5 @@ import slimeknights.tconstruct.library.TinkerRegistry;
 public class ThermalMaterials extends ModuleMaterials {
     @Override
     public void initMaterials(MaterialDatabase database) {
-        //Compatible ore-dict materials:
-        if (isRegistrable("tin")) {
-            TAMaterial tin = database.getMaterial("tin");
-            tin.setRepresentativeItem("ingotTin");
-            TinkerRegistry.addMaterial(tin);
-        }
-
-        if (isRegistrable("aluminum")) {
-            TAMaterial aluminum = database.getMaterial("aluminum");
-            aluminum.setRepresentativeItem("ingotAluminum");
-            TinkerRegistry.addMaterial(aluminum);
-        }
-
-        if (isRegistrable("constantan")) {
-            TAMaterial constantan = database.getMaterial("constantan");
-            constantan.setRepresentativeItem("ingotConstantan");
-            TinkerRegistry.addMaterial(constantan);
-        }
-
-        if (isRegistrable("nickel")) {
-            TAMaterial nickel = database.getMaterial("nickel");
-            nickel.setRepresentativeItem("ingotNickel");
-            TinkerRegistry.addMaterial(nickel);
-        }
-
-        if (isRegistrable("platinum")) {
-            TAMaterial platinum = database.getMaterial("platinum");
-            platinum.setRepresentativeItem("ingotPlatinum");
-            TinkerRegistry.addMaterial(platinum);
-        }
-
-        if (isRegistrable("iridium")) {
-            TAMaterial iridium = database.getMaterial("iridium");
-            iridium.setRepresentativeItem("ingotIridium");
-            TinkerRegistry.addMaterial(iridium);
-        }
-
-        if (isRegistrable("mithril")) {
-            TAMaterial mithril = database.getMaterial("mithril");
-            mithril.setRepresentativeItem("ingotMithril");
-            TinkerRegistry.addMaterial(mithril);
-        }
-
-        if (isRegistrable("invar")) {
-            TAMaterial invar = database.getMaterial("invar");
-            invar.setRepresentativeItem("ingotInvar");
-            TinkerRegistry.addMaterial(invar);
-        }
     }
 }
