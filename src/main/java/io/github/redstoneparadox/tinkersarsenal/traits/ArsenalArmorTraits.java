e1b387ab50532e3ccf39c4dfcd9b7aeec72cbb6e
RedstoneParadox <spinjitzu00@gmail.com>
2020-12-29 15:48:39 -0800

    Alphabetical order plz

--- src/main/java/io/github/redstoneparadox/tinkersarsenal/traits/ArsenalArmorTraits.java
+++ src/main/java/io/github/redstoneparadox/tinkersarsenal/traits/ArsenalArmorTraits.java
@@ -8,19 +8,19 @@ import slimeknights.tconstruct.library.TinkerRegistry;
  * Created by RedstoneParadox on 8/16/2018.
  */
 public class ArsenalArmorTraits {
+    public static final AbstractArmorTrait ANTI_CORROSIVE = new ArmorTraitAntiCorrosive();
+    public static final AbstractArmorTrait CRUMPLING = new ArmorTraitCrumpling();
     public static final AbstractArmorTrait ENDURING = new ArmorTraitEnduring();
+    public static final AbstractArmorTrait FIRE_RESISTANCE = new ArmorTraitFireResistance();
     public static final AbstractArmorTrait RESILIENCE = new ArmorTraitResilience();
     public static final AbstractArmorTrait RIGIDITY = new ArmorTraitRigidity();
-    public static final AbstractArmorTrait ANTI_CORROSIVE = new ArmorTraitAntiCorrosive();
-    public static final AbstractArmorTrait FIRE_RESISTANCE = new ArmorTraitFireResistance();
-    public static final AbstractArmorTrait CRUMPLING = new ArmorTraitCrumpling();
 
     public static void initArmorTraits() {
+        TinkerRegistry.addTrait(ANTI_CORROSIVE);
+        TinkerRegistry.addTrait(CRUMPLING);
         TinkerRegistry.addTrait(ENDURING);
+        TinkerRegistry.addTrait(FIRE_RESISTANCE);
         TinkerRegistry.addTrait(RESILIENCE);
         TinkerRegistry.addTrait(RIGIDITY);
-        TinkerRegistry.addTrait(ANTI_CORROSIVE);
-        TinkerRegistry.addTrait(FIRE_RESISTANCE);
-        TinkerRegistry.addTrait(CRUMPLING);
     }
 }
--- src/main/java/io/github/redstoneparadox/tinkersarsenal/traits/ArsenalToolTraits.java
+++ src/main/java/io/github/redstoneparadox/tinkersarsenal/traits/ArsenalToolTraits.java
@@ -8,24 +8,25 @@ import slimeknights.tconstruct.library.traits.AbstractTrait;
  * Created by RedstoneParadox on 7/29/2018.
  */
 public class ArsenalToolTraits {
-    public static final AbstractTrait MISSINGNO = new TraitMissingno();
     public static final AbstractTrait DIAMOND_EDGE = new TraitDiamondEdge();
     public static final AbstractTrait ENDURING = new TraitEnduring();
-    public static final AbstractTrait RESILIENCE = new TraitResilience();
     public static final AbstractTrait MALLEABLE = new TraitMalleable(1);
+    public static final AbstractTrait MISSINGNO = new TraitMissingno();
+    public static final AbstractTrait RESILIENCE = new TraitResilience();
     // Shaft traits
-    public static final AbstractTrait SWIFT_FLIGHT = new TraitSwiftFlight();
     public static final AbstractTrait GROUNDING = new TraitGrounding();
     public static final AbstractTrait HARD_HITTING = new TraitHardHitting();
+    public static final AbstractTrait SWIFT_FLIGHT = new TraitSwiftFlight();
 
     public static void initToolTraits() {
-        TinkerRegistry.addTrait(MISSINGNO);
         TinkerRegistry.addTrait(DIAMOND_EDGE);
         TinkerRegistry.addTrait(ENDURING);
-        TinkerRegistry.addTrait(RESILIENCE);
         TinkerRegistry.addTrait(MALLEABLE);
-        TinkerRegistry.addTrait(SWIFT_FLIGHT);
+        TinkerRegistry.addTrait(MISSINGNO);
+        TinkerRegistry.addTrait(RESILIENCE);
+
         TinkerRegistry.addTrait(GROUNDING);
         TinkerRegistry.addTrait(HARD_HITTING);
+        TinkerRegistry.addTrait(SWIFT_FLIGHT);
     }
 }
